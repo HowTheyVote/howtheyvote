583f3ecc832a97876c23342758457a2848c86e7d
Till Prochaska <mail@tillprochaska.de>
2020-08-24 22:50:46 +0200

    Build URL from given doc ref

--- ep_votes/document.py
+++ ep_votes/document.py
@@ -2,6 +2,18 @@ import re
 from enum import Enum
 from dataclasses import dataclass
 
+class Type(Enum):
+    A = 1  # report
+    B = 2  # motion for resolution
+    RC = 3 # joint motion for resolution
+
+@dataclass
+class Reference:
+    type: Type
+    term: int
+    number: int
+    year: int
+
 def parse_ref(ref):
     regex = r'^(A|B|RC)(?:-B)?(\d{1,2})-(\d{4})\/(\d{4})$'
     match = re.search(regex, ref)
@@ -16,14 +28,6 @@ def parse_ref(ref):
         year=int(match.group(4))
     )
 
-class Type(Enum):
-    A = 1  # report
-    B = 2  # motion for resolution
-    RC = 3 # joint motion for resolution
-
-@dataclass
-class Reference:
-    type: Type
-    term: int
-    number: int
-    year: int
+def url(ref):
+    BASE = 'https://www.europarl.europa.eu/doceo/document'
+    return f'{BASE}/{ref.type.name}-{ref.term}-{ref.year:04}-{ref.number:04}_EN.html'
