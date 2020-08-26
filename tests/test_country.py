60b27823a6ec16f5a29259fa5daa1992f0b3caa2
Till Prochaska <mail@tillprochaska.de>
2020-08-27 01:10:19 +0200

    Fix alternative country name for CZ

--- tests/test_country.py
+++ tests/test_country.py
@@ -3,3 +3,8 @@ from ep_votes.country import Country
 
 def test_from_str():
     assert Country.from_str("Austria") == Country.AT
+
+
+def test_from_str_alternates():
+    assert Country.from_str("Czech Republic") == Country.CZ
+    assert Country.from_str("Czechia") == Country.CZ
