f36906bea9b29386c931a364edefccde0971b000
Till Prochaska <mail@tillprochaska.de>
2020-08-29 01:33:49 +0200

    Refactor scrapers, tests

--- ep_votes/scrapers.py
+++ ep_votes/scrapers.py
@@ -3,18 +3,19 @@ from bs4 import BeautifulSoup
 import requests
 from io import StringIO
 from datetime import date
+from typing import Set
 from .types import Member, Country
 
 
-class MembersXMLScraper:
+class MembersScraper:
     TERMS = [8, 9]
-    DIRECTORY_BASE_URL = "https://www.europarl.europa.eu/meps/en/directory/xml"
+    DIRECTORY_BASE_URL = "https://europarl.europa.eu/meps/en/directory/xml"
 
     def __init__(self):
         self._members = {}
 
     def run(self):
-        self._download()
+        self._load()
 
         for term in self.TERMS:
             for member in self._get_members(term):
@@ -31,14 +32,14 @@ class MembersXMLScraper:
 
         self._members[web_id].terms = self._members[web_id].terms | member.terms
 
-    def _download(self):
+    def _load(self):
         self._terms = {}
 
         for term in self.TERMS:
-            raw = self._download_term(term)
+            raw = self._load_term(term)
             self._terms[term] = ElementTree.parse(raw)
 
-    def _download_term(self, term):
+    def _load_term(self, term):
         xml = requests.get(self._term_url(term)).text
         return StringIO(xml)
 
@@ -55,20 +56,21 @@ class MembersXMLScraper:
         return Member(europarl_website_id=europarl_website_id, terms={term})
 
 
-class MemberInfoHTMLScraper:
+class MemberInfoScraper:
     PROFILE_BASE_URL = "https://europarl.europa.eu/meps/en"
 
-    def __init__(self, member: Member):
-        self._member = member
+    def __init__(self, europarl_website_id: int, terms: Set[int]):
+        self.europarl_website_id = europarl_website_id
+        self.terms = terms
 
     def run(self):
-        self._download()
+        self._load()
 
         first_name, last_name = Member.parse_full_name(self._full_name())
 
         member = Member(
-            europarl_website_id=self._member.europarl_website_id,
-            terms=set(self._member.terms),
+            europarl_website_id=self.europarl_website_id,
+            terms=set(self.terms),
             first_name=first_name,
             last_name=last_name,
             date_of_birth=self._date_of_birth(),
@@ -77,29 +79,34 @@ class MemberInfoHTMLScraper:
 
         return member
 
-    def _download(self):
+    def _load(self):
         self._terms = {}
 
-        for term in self._member.terms:
-            raw = self._download_term(term)
+        for term in self.terms:
+            raw = self._load_term(term)
             self._terms[term] = BeautifulSoup(raw, "lxml")
 
-    def _download_term(self, term):
+    def _load_term(self, term):
         return requests.get(self._profile_url(term)).text
 
     def _profile_url(self, term):
-        web_id = self._member.europarl_website_id
+        web_id = self.europarl_website_id
         return f"{self.PROFILE_BASE_URL}/{web_id}/NAME/history/{term}"
 
     def _latest_term(self):
-        return self._terms[max(self._member.terms)]
+        return self._terms[max(self.terms)]
 
     def _full_name(self):
         html = self._latest_term()
         return html.select("#presentationmep div.erpl_title-h1")[0].text.strip()
 
     def _date_of_birth(self):
-        raw = self._latest_term().select("#birthDate")[0].text.strip()
+        raw = self._latest_term().select("#birthDate")
+
+        if not raw:
+            return
+
+        raw = raw[0].text.strip()
 
         year = int(raw[6:])
         month = int(raw[3:5])
--- tests/test_scrapers.py
+++ tests/test_scrapers.py
@@ -1,51 +1,58 @@
-from pytest import fixture
-from unittest.mock import patch
+import pytest
+from requests_mock import ANY
 from pathlib import Path
 from datetime import date
-from ep_votes.scrapers import MembersXMLScraper, MemberInfoHTMLScraper
+from ep_votes.scrapers import MembersScraper, MemberInfoScraper
 from ep_votes.types import Member, Country
 
 TEST_DATA_DIR = Path(__file__).resolve().parent / "data"
 
 
-@fixture
-def member():
-    return Member(europarl_website_id=124834, terms={8, 9})
+def mock_response(req, context):
+    url = req.url.replace("https://europarl.europa.eu", "")
+
+    MOCK_RESPONSES = {
+        "/meps/en/directory/xml/?leg=8": "directory_term_8.xml",
+        "/meps/en/directory/xml/?leg=9": "directory_term_9.xml",
+        "/meps/en/124834/NAME/history/8": "sonneborn_term_8.html",
+        "/meps/en/124834/NAME/history/9": "sonneborn_term_9.html",
+        "/meps/en/124831/NAME/history/8": "adinolfi_term_8.html",
+        "/meps/en/124831/NAME/history/9": "adinolfi_term_9.html",
+    }
+
+    file = MOCK_RESPONSES[url]
+    path = TEST_DATA_DIR / file
+
+    return Path(path).read_text()
 
 
-@fixture
-def members_xml_scraper():
-    def mock_download_term(self, term):
-        return open(TEST_DATA_DIR / f"directory_term_{term}.xml")
-
-    with patch.object(MembersXMLScraper, "_download_term", new=mock_download_term):
-        yield MembersXMLScraper()
+@pytest.fixture
+def mock_request(requests_mock):
+    requests_mock.get(ANY, text=mock_response)
+    yield requests_mock
 
 
-@fixture
-def member_info_html_scraper(member):
-    def mock_download_term(self, term):
-        return Path(TEST_DATA_DIR / f"sonneborn_term_{term}.html").read_text()
+def test_members_scraper_run(mock_request):
+    scraper = MembersScraper()
 
-    with patch.object(MemberInfoHTMLScraper, "_download_term", new=mock_download_term):
-        yield MemberInfoHTMLScraper(member=member)
-
-
-def test_members_xml_scraper_run(members_xml_scraper):
     expected = [
         Member(terms={8, 9}, europarl_website_id=1),
         Member(terms={9}, europarl_website_id=2),
     ]
 
-    assert members_xml_scraper.run() == expected
+    assert scraper.run() == expected
 
 
-def test_members_xml_scraper_term_url(members_xml_scraper):
-    expected = "https://www.europarl.europa.eu/meps/en/directory/xml/?leg=9"
-    assert members_xml_scraper._term_url(term=9) == expected
+def test_members_scraper_term_url():
+    scraper = MembersScraper()
+    expected = "https://europarl.europa.eu/meps/en/directory/xml/?leg=9"
+
+    assert scraper._term_url(term=9) == expected
 
 
-def test_member_info_html_scraper_run(member_info_html_scraper):
+def test_member_info_scraper_run(mock_request):
+    scraper = MemberInfoScraper(europarl_website_id=124834, terms={8, 9})
+
     expected = Member(
         europarl_website_id=124834,
         terms={8, 9},
@@ -55,24 +62,35 @@ def test_member_info_html_scraper_run(member_info_html_scraper):
         country=Country.DE,
     )
 
-    assert member_info_html_scraper.run() == expected
+    assert scraper.run() == expected
 
 
-def test_member_info_html_scraper_profile_url(member_info_html_scraper):
+def test_member_info_scraper_profile_url(mock_request):
+    scraper = MemberInfoScraper(europarl_website_id=124834, terms={8, 9})
     expected = "https://europarl.europa.eu/meps/en/124834/NAME/history/9"
-    assert member_info_html_scraper._profile_url(term=9) == expected
+
+    assert scraper._profile_url(term=9) == expected
 
 
-def test_member_info_html_scraper_full_name(member_info_html_scraper):
-    member_info_html_scraper._download()
-    assert member_info_html_scraper._full_name() == "Martin SONNEBORN"
+def test_member_info_scraper_full_name(mock_request):
+    scraper = MemberInfoScraper(europarl_website_id=124834, terms={8, 9})
+    scraper._load()
+    assert scraper._full_name() == "Martin SONNEBORN"
 
 
-def test_member_info_html_scraper_date_of_birth(member_info_html_scraper):
-    member_info_html_scraper._download()
-    assert member_info_html_scraper._date_of_birth() == date(1965, 5, 15)
+def test_member_info_scraper_date_of_birth(mock_request):
+    scraper = MemberInfoScraper(europarl_website_id=124834, terms={8, 9})
+    scraper._load()
+    assert scraper._date_of_birth() == date(1965, 5, 15)
 
 
-def test_member_info_html_scraper_country(member_info_html_scraper):
-    member_info_html_scraper._download()
-    assert member_info_html_scraper._country() == Country.DE
+def test_member_info_scraper_date_of_birth_without(mock_request):
+    scraper = MemberInfoScraper(europarl_website_id=124831, terms={8, 9})
+    scraper._load()
+    assert scraper._date_of_birth() is None
+
+
+def test_member_info_scraper_country(mock_request):
+    scraper = MemberInfoScraper(europarl_website_id=124834, terms={8, 9})
+    scraper._load()
+    assert scraper._country() == Country.DE
