838a7b8e4952bf06b46b98ef66740fcb683d3710
Till Prochaska <mail@tillprochaska.de>
2020-09-20 00:58:58 +0200

    Reduce redundancy

--- app/app/Actions/CompileVoteStatsAction.php
+++ app/app/Actions/CompileVoteStatsAction.php
@@ -4,6 +4,7 @@ namespace App\Actions;
 
 use App\Enums\VotePositionEnum;
 use App\Vote;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
 class CompileVoteStatsAction extends Action
@@ -32,13 +33,7 @@ class CompileVoteStatsAction extends Action
             ->groupBy('country', 'position')
             ->get()
             ->groupBy(fn ($row) => $row->country->label)
-            ->map(function ($group) use ($defaults) {
-                $positions = $group
-                    ->map(fn ($row) => [$row->pivot->position->label, $row->count])
-                    ->toAssoc();
-
-                return $defaults->merge($positions);
-            })
+            ->map(fn ($rows) => $this->formatPositions($rows))
             ->toArray();
     }
 
@@ -46,19 +41,24 @@ class CompileVoteStatsAction extends Action
     {
         $this->log("Compiling general stats for vote {$vote->id}");
 
+        $rows = $vote->members()
+            ->groupBy('position')
+            ->select('position', DB::raw('count(*) as count'))
+            ->get();
+
+        return $this->formatPositions($rows)->toArray();
+    }
+
+    protected function formatPositions(Collection $rows): Collection
+    {
         $defaults = collect(VotePositionEnum::toArray())
             ->map(fn ($position) => [$position, 0])
             ->toAssoc();
 
-        $positions = $vote->members()
-            ->groupBy('position')
-            ->select('position', DB::raw('count(*) as count'))
-            ->get()
+        $positions = $rows
             ->map(fn ($row) => [$row->pivot->position->label, $row->count])
             ->toAssoc();
 
-        return $defaults
-            ->merge($positions)
-            ->toArray();
+        return $defaults->merge($positions);
     }
 }
