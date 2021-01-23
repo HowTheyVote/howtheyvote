a602be909a2ffcc638cb663ac7e4d8697711e0d7
Till Prochaska <mail@tillprochaska.de>
2021-01-23 15:14:50 +0100

    Add artisan commands for scraper actions
    Co-authored-by: linusha <linusha@users.noreply.github.com>
    Signed-off-by: Till Prochaska <mail@tillprochaska.de>

--- app/routes/console.php
+++ app/routes/console.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Actions\ScrapeAndSaveMemberGroupsAction;
+use App\Actions\ScrapeAndSaveMemberInfoAction;
+use App\Actions\ScrapeAndSaveMembersAction;
+use App\Member;
+use App\Term;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,3 +22,29 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('scrape:members {term}', function (int $term, ScrapeAndSaveMembersAction $action) {
+    $term = Term::whereNumber($term)->first();
+    $action->execute($term);
+})->describe('Scrape and save all members (without info) for a given term.');
+
+Artisan::command('scrape:members-info', function (ScrapeAndSaveMemberInfoAction $action) {
+    $allMembers = Member::all();
+    $membersCount = $allMembers->count();
+
+    foreach ($allMembers as $index => $member) {
+        $this->info('Scraping info for member: '.($index + 1)."/{$membersCount}");
+        $action->execute($member);
+    }
+})->describe('Scrape and save info for all saved members.');
+
+Artisan::command('scrape:members-groups {term}', function (int $term, ScrapeAndSaveMemberGroupsAction $action) {
+    $term = Term::whereNumber($term)->first();
+    $allMembers = Member::all();
+    $membersCount = $allMembers->count();
+
+    foreach ($allMembers as $index => $member) {
+        $this->info('Scraping groups for member: '.($index + 1)."/{$membersCount}");
+        $action->execute($member, $term);
+    }
+})->describe('Scrape and save group info for all saved members for the given term.');
