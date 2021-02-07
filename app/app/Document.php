bf1e90979de78d82ee9a99d3412620534def5c34
Till Prochaska <mail@tillprochaska.de>
2021-02-07 11:25:02 +0100

    Add association between document and procedures
    Co-authored-by: Linus Hagemann <linusha@users.noreply.github.com>

--- app/app/Document.php
+++ app/app/Document.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\Enums\DocumentTypeEnum;
+use App\Procedure;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -18,11 +19,17 @@ class Document extends Model
         'number',
         'year',
         'title',
+        'procedure_id',
     ];
 
     protected $casts = [
         'type' => DocumentTypeEnum::class,
     ];
+
+    public function procedure()
+    {
+        return $this->belongsTo(Procedure::class);
+    }
 
     public function term()
     {
