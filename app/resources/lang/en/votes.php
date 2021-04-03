9fe593e2be5c42602f91d840d4d93aadbd29ff46
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-04-03 15:41:14 +0200

    Use localization files for Text on Picture
    Co-authored-by: Till Prochaska <mail@tillprochaska.de>

---
+++ app/resources/lang/en/votes.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'share-picture' => [
+        'footer' => 'Find out how individual MEPs, political groups, and countries voted:',
+        'subtitle' => 'Result of the vote in the European Parliament on :date.',
+        'summary' => 'In total, <strong>:voted MEPs</strong> voted. <strong>:did-not-vote MEPs</strong> didn’t vote.',
+    ],
+    'for' => 'For',
+    'against' => 'Against',
+    'abstentions' => 'Abstentions',
+
+];
--- app/resources/views/components/share-picture.blade.php
+++ app/resources/views/components/share-picture.blade.php
@@ -5,7 +5,7 @@
 
     <footer class="share-picture__footer">
         <div class="share-picture__footer-text">
-            Find out how individual MEPs, political groups, and countries voted:<br>
+            {{ __('votes.share-picture.footer') }}<br>
             <strong>HowTheyVote.eu/abc123</strong>
         </div>
         <x-cc-logo class="share-picture__cc-logo" modifiers="light" />
