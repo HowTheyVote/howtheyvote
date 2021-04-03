dcc76c4fd2e4c3774b937f2de969fe0330faf408
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-04-03 19:22:52 +0200

    Use MariaDB for tests instead of SQLite
    Co-authored-by: Till Prochaska <mail@tillprochaska.de>

--- app/app/Enums/CountryEnum.php
+++ app/app/Enums/CountryEnum.php
@@ -2,6 +2,8 @@
 
 namespace App\Enums;
 
+use Spatie\Enum\Laravel\Enum;
+
 /**
  * @method static self AT()
  * @method static self BE()
--- app/app/Enums/VotePositionEnum.php
+++ app/app/Enums/VotePositionEnum.php
@@ -2,6 +2,8 @@
 
 namespace App\Enums;
 
+use Spatie\Enum\Laravel\Enum;
+
 /**
  * @method static self FOR()
  * @method static self AGAINST()
