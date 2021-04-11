0e0e80ad1b3f15278c22d4f3756a15103da19cc2
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-04-11 20:02:28 +0200

    Add uptime Monitoring for scheduled scrapers
    Co-authored-by: Till Prochaska <mail@tillprochaska.de>

---
+++ app/config/pings.php
@@ -0,0 +1,10 @@
+ <?php
+
+return [
+
+    'scrape-vote-results' => env('PINGS_SCRAPE_VOTE_RESULTS'),
+    'scrape-members' => env('PINGS_SCRAPE_MEMBERS'),
+    'scrape-members-info' => env('PINGS_SCRAPE_MEMBERS_INFO'),
+    'scrape-members-groups' => env('PINGS_SCRAPE_MEMBERS_GROUPS'),
+
+];
