0ac839bd90ba4a2be5ed2fe16cd2baaea42be0e9
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-05-23 12:12:07 +0200

    Extract HTML header into app component
    Co-authored-by: Till Prochaska <mail@tillprochaska.de>

--- app/resources/views/voting-lists/share-picture.blade.php
+++ app/resources/views/voting-lists/share-picture.blade.php
@@ -1,33 +1,20 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@500&family=IBM+Plex+Sans:wght@400;600&display=swap" rel="stylesheet">
-
-        <title>{{ $votingList->display_title }} - Sharepic</title>
-    </head>
-    <body>
-        <x-share-picture>
-            <x-stack space="sm">
-                <x-stack space="xxs">
-                    <h1 class="beta">{{ $votingList->display_title }}</h1>
-                    <p class="text--xs">
-                        @lang('voting-lists.share-picture.subtitle', [
-                            'date' => $votingList->date->formatLocalized('%b %e, %Y'),
-                        ])
-                    </p>
-                </x-stack>
-                <x-vote-result-chart :stats="$votingList->stats" />
+<x-app title="{{ $votingList->displayTitle }} - Share Picture">
+    <x-share-picture>
+        <x-stack space="sm">
+            <x-stack space="xxs">
+                <h1 class="beta">{{ $votingList->display_title }}</h1>
+                <p class="text--xs">
+                    @lang('voting-lists.share-picture.subtitle', [
+                        'date' => $votingList->date->formatLocalized('%b %e, %Y'),
+                    ])
+                </p>
             </x-stack>
+            <x-vote-result-chart :stats="$votingList->stats" />
+        </x-stack>
 
-            <x-slot name="footer">
-                {{ __('voting-lists.share-picture.footer') }}<br>
-                <strong>{{ $shortDisplayUrl }}</strong>
-            </x-slot>
-        </x-share-picture>
-    </body>
-</html>
+        <x-slot name="footer">
+            {{ __('voting-lists.share-picture.footer') }}<br>
+            <strong>{{ $shortDisplayUrl }}</strong>
+        </x-slot>
+    </x-share-picture>
+</x-app>
