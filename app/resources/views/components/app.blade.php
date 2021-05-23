6ddfeb219f002483a6a4dd368493a06516102858
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-05-23 12:12:07 +0200

    Extract HTML header into app component
    Co-authored-by: Till Prochaska <mail@tillprochaska.de>

---
+++ app/resources/views/components/app.blade.php
@@ -0,0 +1,18 @@
+@props(['title' => null])
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@500&family=IBM+Plex+Sans:wght@400;600&display=swap" rel="stylesheet">
+
+        <title>{{ $title }}</title>
+    </head>
+    <body>
+        {{ $slot }}
+    </body>
+</html>
