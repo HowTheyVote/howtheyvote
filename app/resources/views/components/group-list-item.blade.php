fa32eeb96a3f9d415f8384ca891fb63ef11d459b
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-08-04 16:37:10 +0200

    Separate list-items for Members, Countries and Groups
    Co-authored-by: Till Prochaska <mail@tillprochaska.de>

---
+++ app/resources/views/components/group-list-item.blade.php
@@ -0,0 +1,15 @@
+@props([
+    'group' => null,
+    'stats' => null,
+])
+
+<x-list-item
+    :title="$group->name"
+    :subtitle="__('voting-lists.groups.count', [
+        'voted' => $group->stats['voted'],
+        'total' => $group->stats['active'],
+    ])"
+    :stats="$stats"
+    avatarStyle="squared"
+    avatarUrl="/assets/groups/{{ Str::lower($group->code) }}.svg"
+/>
