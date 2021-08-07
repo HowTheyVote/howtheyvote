c889c0bfa08c5fc145b2d40e630f609ff08d7652
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-08-07 14:36:32 +0200

    Add artisan command to scrape all data in a timeframe
    VoteCollections for one session need to be scraped only once, as they are the same for each day of a plenary session. Scraping them all leads to "duplicate" votes that cannot be matched.
    The newly added scrape:all command does handle the this case for one plenary week only.
    Co-authored-by: Till Prochaska <mail@tillprochaska.de>

--- app/config/pings.php
+++ app/config/pings.php
@@ -1,10 +1,5 @@
  <?php
 
 return [
-
-    'scrape-vote-results' => env('PINGS_SCRAPE_VOTE_RESULTS'),
-    'scrape-members' => env('PINGS_SCRAPE_MEMBERS'),
-    'scrape-members-info' => env('PINGS_SCRAPE_MEMBERS_INFO'),
-    'scrape-members-groups' => env('PINGS_SCRAPE_MEMBERS_GROUPS'),
-
+    'scrapers' => env('PINGS_SCRAPERS'),
 ];
--- app/routes/console.php
+++ app/routes/console.php
@@ -9,6 +9,7 @@ use App\Actions\ScrapeVoteCollectionsAction;
 use App\Actions\ScrapeVotingListsAction;
 use App\Member;
 use App\Term;
+use App\VoteCollection;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Artisan;
@@ -98,7 +99,46 @@ Artisan::command('scrape:vote-collections {--term=} {--date=}', function (
     $this->output->writeln('');
 })->describe('Scrape and save all vote collections for the given date and term.');
 
-Artisan::command('match', function (MatchVotesAndVotingListsAction $action) {
+Artisan::command('scrape:all {--term=} {--from=} {--to=}', function (
+    int $term,
+    string $from,
+    string $to
+) {
+    $period = Carbon::parse($from)->toPeriod($to);
+
+    Artisan::call('scrape:members', ['--term' => $term]);
+    Artisan::call('scrape:members-groups', ['--term' => $term]);
+    Artisan::call('scrape:members-infos');
+
+    $count = VoteCollection::count();
+
+    foreach ($period as $date) {
+        Artisan::call('scrape:vote-collections', [
+            '--term' => $term,
+            '--date' => $date,
+        ]);
+
+        // Vote collection documents on the Parliament's website
+        // include all vote collections for the session, including
+        // vote collections that did not take place on the given
+        // day, i.e. we have to make sure to import vote collections
+        // only for the first day of the plenary session.
+        if (VoteCollection::count() > $count) {
+            break;
+        }
+    }
+
+    foreach ($period as $date) {
+        Artisan::call('scrape:voting-lists', [
+            '--term' => $term,
+            '--date' => $date,
+        ]);
+    }
+
+    Artisan::call('scrape:match');
+})->describe('Runs all scrapers for the given date and matches votes and voting lists.');
+
+Artisan::command('scrape:match', function (MatchVotesAndVotingListsAction $action) {
     $action->execute();
 })->describe('Matches all available votes to their voting lists.');
 
