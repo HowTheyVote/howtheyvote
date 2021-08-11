f15fbbc922dad6b205ef1d799cbe08bbbc1416a0
Till Prochaska <mail@tillprochaska.de>
2021-08-11 13:20:11 +0200

    Display summary

---
+++ app/tests/Unit/SummaryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Summary;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(Tests\TestCase::class, RefreshDatabase::class);
+
+it('has excerpt', function () {
+    $summary = Summary::factory([
+        'text' => "The European Parliament adopted by 667 votes to 1, with 27 abstentions, a resolution on the situation in Myanmar.\n\nThe text adopted in plenary had been tabled as a joint resolution by the EPP, S&D, Renew, Greens/EFA, ECR groups and The Left.\n\nOn 1 February 2021, the military of Myanmar, known as the Tatmadaw, in a clear violation of the constitution of Myanmar, arrested President Win Myint and State Counsellor Aung San Suu Kyi, as well as leading members of the government, seized power over the legislative, judicial and executive branches of government through a coup d’état, and issued a one-year state of emergency.",
+    ])->make();
+
+    $expected = "The text adopted in plenary had been tabled as a joint resolution by the EPP, S&D, Renew, Greens/EFA, ECR groups and The Left.\n\nOn 1 February 2021, the military of Myanmar, known as the Tatmadaw, in a clear violation of the constitution of Myanmar, arrested President Win Myint and State...";
+
+    expect($summary->excerpt)->toEqual($expected);
+});
