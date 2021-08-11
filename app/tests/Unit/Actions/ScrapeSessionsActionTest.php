b04afadf5b51d9acaf132d9d3900bbda4a8dea8c
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-08-11 18:30:36 +0200

    Add ScrapeSessionAction and Sessions Table

---
+++ app/tests/Unit/Actions/ScrapeSessionsActionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Actions\ScrapeSessionsAction;
+use App\Enums\LocationEnum;
+use App\Session;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(Tests\TestCase::class, RefreshDatabase::class);
+
+beforeEach(function () {
+    $this->action = $this->app->make(ScrapeSessionsAction::class);
+
+    Http::fakeJsonFromFile('*/sessions?year=2021&month=11', 'sessions.json');
+});
+
+it('creates new session records', function () {
+    $this->action->execute(2021, 11);
+
+    expect(Session::count())->toEqual(2);
+
+    $session = Session::find(1);
+
+    expect($session->start_date)->toEqual('2021-11-10');
+    expect($session->end_date)->toEqual('2021-11-11');
+    expect($session->location)->toEqual(LocationEnum::BRUSSELS());
+
+    $session = Session::find(2);
+
+    expect($session->start_date)->toEqual('2021-11-22');
+    expect($session->end_date)->toEqual('2021-11-25');
+    expect($session->location)->toEqual(LocationEnum::STRASBOURG());
+});
