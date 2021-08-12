772a0b5a88fe81e8750ca29c95f312a54c062ad9
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-08-12 16:31:58 +0200

    Update DB dev-dump to contain Summaries and Sessions

--- mariadb/dump/export_database.sh
+++ mariadb/dump/export_database.sh
@@ -3,4 +3,4 @@
 # https://stackoverflow.com/a/246128
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-docker-compose exec mariadb mysqldump -u laravel -plaravel laravel > "$DIR"/dump.sql
+docker-compose exec mariadb mysqldump -u laravel -plaravel laravel > "$DIR"/dump-development.sql
--- mariadb/dump/import_database.sh
+++ mariadb/dump/import_database.sh
@@ -3,4 +3,4 @@
 # https://stackoverflow.com/a/246128
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-docker-compose exec -T mariadb mysql -u laravel -plaravel laravel < "$DIR"/dump.sql
+docker-compose exec -T mariadb mysql -u laravel -plaravel laravel < "$DIR"/dump-development.sql
