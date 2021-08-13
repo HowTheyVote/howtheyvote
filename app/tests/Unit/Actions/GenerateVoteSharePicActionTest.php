e1c0616edad732d556bf880a8ba9a22aa49b9e57
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-08-13 14:29:50 +0200

    Add tests for GenerateVoteSharePicAction

---
+++ app/tests/Unit/Actions/GenerateVoteSharePicActionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Actions\GenerateVoteSharePicAction;
+use App\Vote;
+use App\VotingList;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(Tests\TestCase::class, RefreshDatabase::class);
+
+beforeEach(function () {
+    Storage::fake('public');
+
+    $this->action = new GenerateVoteSharePicAction();
+
+    $this->votingList = VotingList::factory([
+        'vote_id' => Vote::factory(),
+    ])->withStats()->create();
+});
+
+it('uploads an image', function () {
+    $this->action->execute($this->votingList);
+
+    expect(Storage::disk('public')->exists("vote-sharepic-{$this->votingList->id}.png"))->toEqual(true);
+});
+
+it('does not create a new one if a share-pic already exists', function () {
+    $this->action->execute($this->votingList);
+
+    $modifiedTimestamp = Storage::disk('public')->lastModified("vote-sharepic-{$this->votingList->id}.png");
+
+    $this->action->execute($this->votingList);
+
+    expect(Storage::disk('public')->lastModified("vote-sharepic-{$this->votingList->id}.png"))->toEqual($modifiedTimestamp);
+});
