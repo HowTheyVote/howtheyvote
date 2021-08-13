328e684c345a8fe74df1ea472064d903838da9f4
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-08-13 18:37:26 +0200

    Add shareButton on vote site for primary votes

---
+++ app/resources/views/components/share-button.blade.php
@@ -0,0 +1,19 @@
+@props([
+    'title'=>null,
+    'text'=>null,
+    'url'=>null,
+])
+
+<x-button
+    x-data="{
+        isVisible: 'share' in navigator,
+        title: '{{ $title }}',
+        text: '{{ $text }}',
+        url: '{{ $url }}'
+    }"
+    x-on:click="navigator.share({title, text, url})"
+    x-show="isVisible"
+    x-cloak
+>
+Share this vote!
+</x-button>
