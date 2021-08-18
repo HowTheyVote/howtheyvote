6bb3659d28fa2f0dc0831a5dc6a4b601b9c3d5a0
Till Prochaska <mail@tillprochaska.de>
2021-08-18 14:30:45 +0200

    Replace Google Fonts with self hosted fonts
    Closes #426

--- app/resources/views/components/app.blade.php
+++ app/resources/views/components/app.blade.php
@@ -12,9 +12,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@500&family=IBM+Plex+Sans:wght@400;600&display=swap" rel="stylesheet">
-
         <script src="{{ mix('js/app.js') }}" async defer></script>
 
         <title>{{ $title }}</title>
