6988e4328389affdf3dbc66b9d29a82753da2eef
Till Prochaska <mail@tillprochaska.de>
2021-08-20 22:32:01 +0200

    Localize search component

--- app/resources/lang/en/components.php
+++ app/resources/lang/en/components.php
@@ -6,4 +6,9 @@ return [
         'show-less' => 'Show less',
         'search-placeholder' => 'Search',
     ],
+    'search' => [
+        'results' => 'Results',
+        'load-more' => 'Load more',
+        'placeholder' => 'Search votes by subject, e.g. COVID-19',
+    ],
 ];
