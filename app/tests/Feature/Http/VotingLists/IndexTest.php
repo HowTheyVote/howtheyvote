2d6742cb512b688b79345d6cd491fd648ebb9e74
Till Prochaska <mail@tillprochaska.de>
2021-08-24 18:27:20 +0200

    Temporarily remove tests for index page
    The new index page is mostly rendered on the client-side, i.e. we’d need to set up browser testing.

--- app/tests/Feature/Http/VotingLists/IndexTest.php
+++ app/tests/Feature/Http/VotingLists/IndexTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Enums\LocationEnum;
-use App\Enums\VoteTypeEnum;
 use App\Session;
 use App\Vote;
 use App\VotingList;
@@ -26,56 +25,4 @@ beforeEach(function () {
 it('renders successfully', function () {
     $response = $this->get('/votes');
     expect($response)->toHaveStatus(200);
-});
-
-it('shows a vote card for each existing final vote', function () {
-    $response = $this->get('/votes');
-    expect($response)->toHaveSelector('.vote-card', 2);
-});
-
-it('renders the session titles', function () {
-    $response = $this->get('/votes');
-    expect($response)->toHaveSelectorWithText('.beta', 'January 2021 · Brussels');
-});
-
-it('does not include final votes that have not been matched to a voting list', function () {
-    Vote::factory([
-            'session_id' => Session::first(),
-            'type' => VoteTypeEnum::PRIMARY(),
-            'final' => true,
-    ])->create();
-
-    Vote::factory([
-        'session_id' => Session::first(),
-        'type' => VoteTypeEnum::SEPARATE(),
-        'final' => true,
-    ])->create();
-
-    $response = $this->get('/votes');
-    expect($response)->toHaveSelector('.vote-card', 2);
-});
-
-it('does not show session-headings for sessions without matched final votes', function () {
-    // matched but not final
-    VotingList::factory([
-        'vote_id' => Vote::factory([
-            'session_id' => Session::factory([
-                'start_date' => '2021-01-05',
-                'end_date' => '2021-01-08',
-                'location' => LocationEnum::BRUSSELS(),
-            ])->create(),
-        ]),
-    ])->create();
-
-    // final but not matched
-    Vote::factory([
-            'session_id' => Session::factory([
-                'start_date' => '2021-03-05',
-                'end_date' => '2021-03-08',
-                'location' => LocationEnum::BRUSSELS(),
-            ]),
-    ])->create();
-
-    $response = $this->get('/votes');
-    expect($response)->toHaveSelector('.beta', 1);
 });
