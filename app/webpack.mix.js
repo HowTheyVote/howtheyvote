e995a4dca7ecb6bba78695050d6d95301baf325a
Till Prochaska <mail@tillprochaska.de>
2021-08-26 01:28:30 +0200

    Setup browsersync to automatically update CSS/JS on changes

--- app/webpack.mix.js
+++ app/webpack.mix.js
@@ -2,3 +2,7 @@ const mix = require('laravel-mix');
 
 mix.postCss('resources/css/app.css', 'public/css').version();
 mix.js('resources/js/app.js', 'public/js').version();
+mix.browserSync({
+  proxy: 'localhost:8000',
+  open: false,
+});
