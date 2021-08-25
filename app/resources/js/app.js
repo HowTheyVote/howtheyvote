f78bc8e6d52cd5ed18e014bc40ba20f97507ce26
Till Prochaska <mail@tillprochaska.de>
2021-08-25 13:45:48 +0200

    Prefetch in-viewport links using quicklink package

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,7 +1,10 @@
 import '@github/tab-container-element';
+import { listen } from 'quicklink';
 import Alpine from 'alpinejs';
 import listItem from './components/list-item.js';
 import search from './components/search.js';
+
+window.addEventListener('load', () => listen());
 
 Alpine.data('listItem', listItem);
 Alpine.data('search', search);
