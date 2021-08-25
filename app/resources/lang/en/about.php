dea9f7b998425141321bdaae945d6766f07792a0
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-08-25 11:32:31 +0200

    Add licenses on /about page

--- app/resources/lang/en/about.php
+++ app/resources/lang/en/about.php
@@ -2,6 +2,13 @@
 
 return [
     'title' => 'About',
+    'headings' => [
+        'license' => 'Licenses',
+        'funding' => 'Funding',
+        'ep-copyright' => 'Copyright Notice of the European Parliament',
+    ],
     'text' => 'HowTheyVote.eu is an tool to easily research the voting behaviour of politicians in the European Parliament. Based on the raw voting data as published on the parliaments website, we let you search votes, show their outcome and how each member of parliament voted.',
     'funding' => 'This work has been funded by the German Federal Ministry of Research and Education as part of the 9th round of the <a href="https://prototypefund.de" target="_blank" rel="noopener noreferrer" />Prototype Fund</a> under the funding reference 1IS21818.',
+    'copyright-ep' => 'The <a href="https://oeil.secure.europarl.europa.eu/oeil/home/home.do" target="_blank" rel="noopener noreferrer">summaries</a> of vote subjects, <a href="https://www.europarl.europa.eu/plenary/en/minutes.html" target="_blank" rel="noopener noreferrer">voting-data</a>, and <a href="https://www.europarl.europa.eu/meps/en/directory" target="_blank" rel="noopener noreferrer">MEP pictures</a> are sourced from websites of the European Parliament. © European Union – Source: European Parliament. For more information regarding the content of the European Parliament, see their <a href="https://www.europarl.europa.eu/legal-notice/en/" target="_blank" rel="noopener noreferrer">copyright notice</a>.',
+    'our-license' => 'All remaining content on this site is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode" target="_blank" rel="noopener noreferrer">CC-BY-SA 4.0</a>.',
 ];
--- app/resources/views/about.blade.php
+++ app/resources/views/about.blade.php
@@ -4,11 +4,14 @@
 
             <h1 class="alpha">{{ __('about.title') }}</h1>
 
-            <x-stack space="lg">
-                <p>{!! __('about.text')  !!}</p>
+            <p>{!! __('about.text')  !!}</p>
 
-                <p>{!! __('about.funding')  !!}</p>
-            </x-stack>
+            <h2 class="beta">{{ __('about.headings.license')}}</h2>
+            <p>{!! __('about.copyright-ep')  !!}</p>
+            <p>{!! __('about.our-license')  !!}</p>
+
+            <h2 class="beta">{{ __('about.headings.funding')}}</h2>
+            <p>{!! __('about.funding')  !!}</p>
 
             <x-logos>
                 <img src="/assets/logos/logo-bmbf.svg" alt="Federal Ministry of Education and Research">
