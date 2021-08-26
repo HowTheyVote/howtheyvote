3b27cf20e14bc315dcc6a103ac51b987cdec5214
Till Prochaska <mail@tillprochaska.de>
2021-08-26 16:46:03 +0200

    Adjust about view

--- app/resources/views/about.blade.php
+++ app/resources/views/about.blade.php
@@ -1,32 +1,37 @@
 <x-app :title="__('about.title')">
     <x-base-layout>
-        <x-stack>
+        <div class="padding--h padding--v">
+            <x-wrapper>
+                <x-stack>
+                    <h1 class="alpha">{{ __('about.title') }}</h1>
 
-            <h1 class="alpha">{{ __('about.title') }}</h1>
+                        <x-markdown>
+{{ __('about.parliament')  }}
 
-            <x-stack space="sm">
-                    <p>{{ __('about.parliament')  }}</p>
-                    <p>{{ __('about.parliamentary-informations')  }}</p>
-                    <p>{{ __('about.goal')  }}</p>
-            </x-stack>
+{{ __('about.parliamentary-informations')  }}
 
-            <h2 class="beta">{{ __('about.headings.contact')}}</h2>
-            <p>{!! __('about.outreach')  !!}</p>
-            <p>{{ __('about.disclaimer')  }}</p>
+{{ __('about.goal')  }}
 
-            <h2 class="beta">{{ __('about.headings.license')}}</h2>
-            <p>{!! __('about.copyright-ep')  !!}</p>
-            <p>{!! __('about.our-license')  !!}</p>
+{{ __('about.headings.contact') }}
+{{ __('about.outreach')  }}
 
-            <h2 class="beta">{{ __('about.headings.funding')}}</h2>
-            <p>{!! __('about.funding')  !!}</p>
+{{ __('about.disclaimer')  }}
 
-            <x-logos>
-                <img src="/assets/logos/logo-bmbf.svg" alt="Federal Ministry of Education and Research">
+{{ __('about.headings.license')}}
+{{ __('about.copyright-ep')  }}
 
-                <img src="/assets/logos/logo-ptf.svg" alt="Prototype Fund">
-            </x-logos>
+{{ __('about.our-license')  }}
 
-        </x-stack>
+{{ __('about.headings.funding') }}
+{{ __('about.funding')  }}
+                        </x-markdown>
+
+                    <x-logos>
+                        <img src="/assets/logos/logo-bmbf.svg" alt="Federal Ministry of Education and Research">
+                        <img src="/assets/logos/logo-ptf.svg" alt="Prototype Fund">
+                    </x-logos>
+                </x-stack>
+            </x-wrapper>
+        </div>
     </x-base-layout>
 </x-app>
