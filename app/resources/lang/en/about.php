2dd465da1376203e0d51da13fd9076ee95e2ff0e
linusha <linus.hagemann@student.hpi.de>
2021-08-26 12:15:27 +0200

    Add text on about page and license to README

--- app/resources/lang/en/about.php
+++ app/resources/lang/en/about.php
@@ -2,6 +2,18 @@
 
 return [
     'title' => 'About',
-    'text' => 'HowTheyVote.eu is an tool to easily research the voting behaviour of politicians in the European Parliament. Based on the raw voting data as published on the parliaments website, we let you search votes, show their outcome and how each member of parliament voted.',
+    'headings' => [
+        'license' => 'Licenses',
+        'funding' => 'Funding',
+        'contact' => 'Contact Us',
+        'ep-copyright' => 'Copyright Notice of the European Parliament',
+    ],
+    'parliament' => 'The European Union is one of the largest democracies in the world. The European Parliament, with its 705 members from the EU\'s 27 member states, represents just over 440 million Europeans. Although European politics affects so many people, it often receives little public attention. Yet transparency of parliamentary processes is a fundamental prerequisite for forming opinions in democratic discourse.',
+    'parliamentary-informations' => 'Information such as agendas, minutes and the results of roll-call votes are generally available on the website of the European Parliament. However, it is rather difficult to find out what MEPs voted on or how a particular vote turned out. This is because the necessary information has to be gathered from various websites of the Parliament.',
+    'goal' => 'Our goal is to provide easy access to the voting results of the European Parliament. On this website, we clearly present votes and voting results as well as the voting behavior of individual MEPs. For this purpose, we automatically collect data from the various official parliamentary sources.',
+    'outreach' => 'If you use the data from HowTheyVote.eu for your own projects, we would love to hear from you! And if you have an idea how HowTheyVote.eu could be even more helpful, feel free to open a ticket in our GitHub <a href="https://github.com/HowTheyVote/epvotes" target="_blank" rel="noopener noreferrer">repository</a> or write us an <a href="mailto:mail@howtheyvote.eu" target="_blank" rel="noopener noreferrer">email</a> or on Twitter <a href="https://twitter.com/HowTheyVoteEU" target="_blank" rel="noopener noreferrer">@HowTheyVoteEU.</a>',
+    'disclaimer' => 'The page is still in an early phase. We will extend the available data in the future and continue working on additional functionality. If you find a mistake (e.g., wrong data for a vote) or something is not working as expected, we would appreciate if you contact us or open an issue in our <a href="https://github.com/HowTheyVote/epvotes" target="_blank" rel="no referrer noppender">GitHub repository</a>.',
     'funding' => 'This work has been funded by the German Federal Ministry of Research and Education as part of the 9th round of the <a href="https://prototypefund.de" target="_blank" rel="noopener noreferrer" />Prototype Fund</a> under the funding reference 1IS21818.',
+    'copyright-ep' => 'The <a href="https://oeil.secure.europarl.europa.eu/oeil/home/home.do" target="_blank" rel="noopener noreferrer">summaries</a> of vote subjects, <a href="https://www.europarl.europa.eu/plenary/en/minutes.html" target="_blank" rel="noopener noreferrer">voting data</a>, and <a href="https://www.europarl.europa.eu/meps/en/directory" target="_blank" rel="noopener noreferrer">photos of MEPs</a> are sourced from websites of the European Parliament. © European Union – Source: European Parliament. Please refer to the Parliament’s <a href="https://www.europarl.europa.eu/legal-notice/en/" target="_blank" rel="noopener noreferrer">copyright notice</a>.',
+    'our-license' => 'All remaining content on this site is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode" target="_blank" rel="noopener noreferrer">CC-BY-SA 4.0</a>.',
 ];
