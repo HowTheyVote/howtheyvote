8bd8bbf2c972ba33a29fba81adcd4d924b59fdd6
Till Prochaska <mail@tillprochaska.de>
2021-08-26 16:49:42 +0200

    Adjust imprint view

--- app/resources/views/imprint.blade.php
+++ app/resources/views/imprint.blade.php
@@ -1,22 +1,26 @@
 <x-app :title="__('imprint.title')">
     <x-base-layout>
-        <x-stack>
-            <h1 class="alpha">
-                {{ __('imprint.title') }}
-            </h1>
+        <div class="padding--h padding--v">
+            <x-wrapper>
+                <x-stack>
+                    <h1 class="alpha">
+                        {{ __('imprint.title') }}
+                    </h1>
 
-            <x-stack space="sm">
-                <p>
-                    {{ __('imprint.name') }}<br>
-                    {{ __('imprint.street') }}<br>
-                    {{ __('imprint.city') }}
-                </p>
-                <p>
-                    {{ __('imprint.email') }}<br>
-                    {{ __('imprint.tax-id') }}
-                </p>
-                <p>{{ __('imprint.privacy') }}</p>
-            </x-stack>
-        </x-stack>
+                    <x-stack space="sm">
+                        <p>
+                            {{ __('imprint.name') }}<br>
+                            {{ __('imprint.street') }}<br>
+                            {{ __('imprint.city') }}
+                        </p>
+                        <p>
+                            {{ __('imprint.email') }}<br>
+                            {{ __('imprint.tax-id') }}
+                        </p>
+                        <p>{{ __('imprint.privacy') }}</p>
+                    </x-stack>
+                </x-stack>
+            </x-wrapper>
+        </div>
     </x-base-layout>
 </x-app>
