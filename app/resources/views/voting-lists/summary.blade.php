b58cb642efc1166488eb674bf833f206f761a0e1
Till Prochaska <mail@tillprochaska.de>
2021-08-27 12:10:05 +0200

    Adjust summary view

--- app/resources/views/voting-lists/summary.blade.php
+++ app/resources/views/voting-lists/summary.blade.php
@@ -1,20 +1,31 @@
 <x-app title="{{ $votingList->displayTitle }} · Summary">
     <x-base-layout>
         <x-stack>
-            <h1 class="alpha">{{ $votingList->display_title }}</h1>
+            <x-voting-list.header
+                :votingList="$votingList"
+                :minimal="true"
+            />
 
-            <x-markdown>
-                {{ $summary->text }}
-            </x-markdown>
+            <div class="padding--h">
+                <x-wrapper>
+                    <x-markdown>
+                        {{ $summary->text }}
+                    </x-markdown>
+                </x-wrapper>
+            </div>
 
-            <x-callout>
-                <p>
-                    {!! __('votes.summary.copyright', [
-                        'url' => $summary->external_url,
-                        'year' => $summary->created_at->format('Y'),
-                    ]) !!}
-                </p>
-            </x-callout>
+            <div class="padding--h">
+                <x-wrapper>
+                    <x-callout>
+                        <p>
+                            {!! __('votes.summary.copyright', [
+                                'url' => $summary->external_url,
+                                'year' => $summary->created_at->format('Y'),
+                            ]) !!}
+                        </p>
+                    </x-callout>
+                </x-wrapper>
+            </div>
         </x-stack>
     </x-base-layout>
 </x-app>
