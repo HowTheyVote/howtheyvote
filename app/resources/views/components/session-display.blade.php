05d305ccc4d9acf08501d511c3a58fccfc9613bc
Till Prochaska <mail@tillprochaska.de>
2021-08-27 17:15:07 +0200

    Display dates of last or upcoming plenary sessions on home page

---
+++ app/resources/views/components/session-display.blade.php
@@ -0,0 +1,42 @@
+@props([
+    'currentSession' => null,
+    'lastSession' => null,
+    'nextSession' => null,
+])
+
+
+ @if ($currentSession)
+
+    <div {{ $attributes->bem('session-display') }}>
+        {{ __('components.session-display.current', [
+            'location' => $currentSession->location->label,
+        ]) }}
+
+        {{ __('components.session-display.availability') }}
+    </div>
+
+@elseif ($lastSession && $lastSession->votes()->matched()->final()->count() <= 0)
+
+    <div {{ $attributes->bem('session-display') }}>
+        {!! __('components.session-display.last', [
+            'start' => $lastSession->start_date->format('M jS'),
+            'end' =>$lastSession->end_date->format('M jS'),
+            'location' => $lastSession->location->label,
+        ]) !!}
+
+        {{ __('components.session-display.availability') }}
+    </div>
+
+@elseif ($nextSession)
+
+    <div {{ $attributes->bem('session-display') }}>
+        {!! __('components.session-display.next', [
+            'start' => $nextSession->start_date->format('M jS'),
+            'end' =>$nextSession->end_date->format('M jS'),
+            'location' => $nextSession->location->label,
+        ]) !!}
+
+        {{ __('components.session-display.availability') }}
+    </div>
+
+@endif
