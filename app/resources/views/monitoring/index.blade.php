5144997dbabb950a63e9f5747b7f1ecf7e4783e8
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-08-27 14:46:50 +0200

    Make more information accessible on /monitoring and monitor single items

--- app/resources/views/monitoring/index.blade.php
+++ app/resources/views/monitoring/index.blade.php
@@ -1,9 +1,28 @@
 <x-app title="Monitoring">
     <x-base-layout>
-        <ul>
-            @foreach ($votingLists as $votingList)
-                <li>{{ $votingList->id }} - {{ $votingList->description }}</li>
-            @endforeach
-        </ul>
+        <x-stack>
+            <h1>VotingLists</h1>
+            <ul>
+                @foreach ($votingLists as $votingList)
+                    <li>
+                        <a href="{{route('monitoring.showLists', [
+                            'votingList' => $votingList])}}">
+                                {{ $votingList->id }} - {{ $votingList->description }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+            <h1>Votes</h1>
+            <ul>
+                @foreach ($votes as $vote)
+                    <li>
+                        <a href="{{route('monitoring.showVotes', [
+                            'vote' => $vote])}}">
+                            {{ $vote->id }} - Subject: {{ $vote-subject }} - Collection: {{ $vote->vote_collection_id }} - Reference: {{ $vote->reference }} - Notes: {{ $vote->notes }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        </x-stack>
     </x-base-layout>
 </x-app>
---
+++ app/resources/views/monitoring/show.blade.php
@@ -0,0 +1,5 @@
+<x-app title="Monitoring">
+    <x-base-layout>
+        {{ dd($record) }}
+    </x-base-layout>
+</x-app>
