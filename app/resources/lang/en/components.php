ec512ed6ac399ffa4ecdcee4d87a708062e7107f
Till Prochaska <mail@tillprochaska.de>
2021-08-27 18:47:13 +0200

    Remove unnecessary translation

--- app/resources/lang/en/components.php
+++ app/resources/lang/en/components.php
@@ -37,6 +37,5 @@ return [
         'current' => 'Parliament is meeting in :location this week.',
         'last' => 'The last plenary session was held <strong>from :start to :end</strong> in :location.',
         'next' => 'The next plenary session will be held in :location <strong>from :start to :end</strong>.',
-        'unknown' => 'We currently do not have the dates for the next plenary session.',
     ],
 ];
