16398c922b6da7e75ecc39a1bd7fcb9e23724a46
Till Prochaska <mail@tillprochaska.de>
2021-08-27 17:33:16 +0200

    Add static helpers to get current, next, last session

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -3,34 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Session;
-use Illuminate\Support\Carbon;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $today = Carbon::now();
-
-        $currentSession = Session::query()
-            ->where('start_date', '<=', $today)
-            ->where('end_date', '>=', $today)
-            ->first();
-
-        $lastSession = Session::query()
-            ->where('start_date', '<=', $today)
-            ->where('end_date', '<', $today)
-            ->orderByDesc('start_date')
-            ->first();
-
-        $nextSession = Session::query()
-            ->where('start_date', '>', $today)
-            ->orderBy('start_date')
-            ->first();
-
         return view('home', [
-            'currentSession' => $currentSession,
-            'lastSession' => $lastSession,
-            'nextSession' => $nextSession,
+            'currentSession' => Session::current(),
+            'lastSession' => Session::last(),
+            'nextSession' => Session::next(),
         ]);
     }
 }
