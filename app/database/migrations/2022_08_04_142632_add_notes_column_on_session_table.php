9c38cc4b14b21f291d718034d83062e96d49b3cf
Linus Hagemann <linus.hagemann@posteo.net>
2022-08-04 18:25:38 +0200

    Add `note` column on Session model
    This is a field intended for administrative notes. It was created after discovering that some sessions do actually not contain any votes. It makes sense to allow this information to be saved informally.

---
+++ app/database/migrations/2022_08_04_142632_add_notes_column_on_session_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNotesColumnOnSessionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sessions', function (Blueprint $table) {
+            $table->string('notes')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
