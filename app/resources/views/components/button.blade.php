e389e94e2e53d8adaaf896a0af69eaf96de478e0
Till Prochaska <till.prochaska@occrp.org>
2022-08-13 19:23:06 +0200

    Add position select component

--- app/resources/views/components/button.blade.php
+++ app/resources/views/components/button.blade.php
@@ -2,12 +2,17 @@
     'size' => null,
     'href' => null,
     'style' => null,
+    'tag' => null,
 ])
 
 @if ($href)
-    <a {{ $attributes->bem('button', $size) }} href="{{ $href }}">
+    <a {{ $attributes->bem('button', [$size, $style]) }} href="{{ $href }}">
         {{ $slot }}
     </a>
+@elseif ($tag)
+    <{{ $tag }} {{ $attributes->bem('button', [$size, $style]) }}>
+        {{ $slot }}
+    </{{ $tag }}>
 @else
     <button {{ $attributes->bem('button', [$size, $style]) }}>
         {{ $slot }}
