73e0ffe1ad6f78c337f585782dfc2c614aa8b7f8
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2024-11-17 14:02:39 +0100

    Optionally disable all HTTP request mocks

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -5,8 +5,10 @@ running tests and use pytest hooks to start a transaction before individual test
 executed and rollback any changes after execution. In order to test API routes, we make use
 of Flask’s built-in test client."""
 
+import os
+
 import pytest
-import responses as responses_lib
+from responses import FirstMatchRegistry, RequestsMock
 
 from howtheyvote.db import Session, engine, migrate, session_factory
 from howtheyvote.meili import configure_indexes, delete_indexes
@@ -55,8 +57,33 @@ def api(app):
     yield app.test_client()
 
 
+class DummyRegistry(FirstMatchRegistry):
+    """A registry that ignores any requests that are added."""
+
+    def add(self, response):
+        return response
+
+
 @pytest.fixture
 def responses():
     """Allows mocking HTTP requests made with requests."""
-    with responses_lib.RequestsMock() as r:
-        yield r
+
+    mock_requests = os.environ.get("HTV_TEST_MOCK_REQUESTS", "true").lower() in ["true", "1"]
+
+    if not mock_requests:
+        # In most cases, we want HTTP requests in tests to be mocked. The `responses` package
+        # doesn’t seem to provide a global configuration option to disable all mocks and pass
+        # through the request.
+        #
+        # When calling `responses.get("http://...", body="Lorem ipsum")` in a test to register
+        # a mock response, the mock is stored in a registry. When the tested then tries to send
+        # a matching request, `responses` tries to find a matching mock in the registry. To
+        # disable all mocks, we simply pass a dummy registry that never actually registers any
+        # mocks and allow all unmatched requests to pass to the original source.
+        with RequestsMock(registry=DummyRegistry) as r:
+            r.add_passthru("http")
+            yield r
+    else:
+        # Return a "normal" requests mock that fails any request that isn’t explicitly mocked.
+        with RequestsMock() as r:
+            yield r
