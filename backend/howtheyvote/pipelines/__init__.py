67c23540d059b2b7adaa97b9ddc83fdf3c99e934
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2024-12-07 18:41:02 +0100

    Add common base class for all pipelines
    This is analogous to the `BaseScraper` class for scrapers. In contrast to the scrapers, pipelines don’t share a lot of code (except for some error handling and logging), so there wasn’t really a need to add this before. However, having a common base class is also helpful for typing.

--- backend/howtheyvote/pipelines/__init__.py
+++ backend/howtheyvote/pipelines/__init__.py
@@ -1,10 +1,11 @@
-from .common import DataUnavailableError, DataUnchangedError, PipelineError
+from .common import BasePipeline, DataUnavailableError, DataUnchangedError, PipelineError
 from .members import MembersPipeline
 from .press import PressPipeline
 from .rcv_list import RCVListPipeline
 from .sessions import SessionsPipeline
 
 __all__ = [
+    "BasePipeline",
     "PipelineError",
     "DataUnavailableError",
     "DataUnchangedError",
--- backend/howtheyvote/pipelines/common.py
+++ backend/howtheyvote/pipelines/common.py
@@ -1,3 +1,13 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+from structlog import get_logger
+
+from ..scrapers import ScrapingError
+
+log = get_logger(__name__)
+
+
 class PipelineError(Exception):
     pass
 
@@ -8,3 +18,25 @@ class DataUnavailableError(PipelineError):
 
 class DataUnchangedError(PipelineError):
     pass
+
+
+class BasePipeline(ABC):
+    last_run_checksum: str | None
+    checksum: str | None
+
+    def __init__(self, last_run_checksum: str | None = None, **kwargs: Any) -> None:
+        self.last_run_checksum = last_run_checksum
+        self.checksum = None
+        self._log = log.bind(pipeline=type(self).__name__, **kwargs)
+
+    def run(self) -> None:
+        self._log.info("Running pipeline")
+
+        try:
+            self._run()
+        except ScrapingError:
+            self._log.exception("Failed running pipeline")
+
+    @abstractmethod
+    def _run(self) -> None:
+        raise NotImplementedError
