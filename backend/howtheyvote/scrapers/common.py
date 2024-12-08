4189b40d25f20ea68e162cca1d2a4482392acbab
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2024-12-08 16:09:27 +0100

    Refactor: Don’t compute checksums in scraper
    I initially added some code to `BaseScraper` that automatically computes the response checksum and exposes it as a property. This is only ever used by pipelines, so in order to remove some indirection, I’m moving it to a pipelines helper.

--- backend/howtheyvote/scrapers/common.py
+++ backend/howtheyvote/scrapers/common.py
@@ -1,4 +1,3 @@
-import hashlib
 import html
 import random
 import time
@@ -95,18 +94,15 @@ ResourceType = TypeVar("ResourceType")
 
 class BaseScraper(ABC, Generic[ResourceType]):
     REQUEST_MAX_RETRIES: int = 0
-    response_checksum: str | None
 
     def __init__(self, request_cache: RequestCache | None = None, **kwargs: Any) -> None:
         self._request_cache = request_cache
         self._log = log.bind(scraper=type(self).__name__, **kwargs)
-        self.response_checksum = None
 
     def run(self) -> Any:
         self._log.info("Running scraper")
-        self._response = self._fetch()
-        self.response_checksum = self._compute_checksum(self._response)
-        doc = self._parse(self._response)
+        self.response = self._fetch()
+        doc = self._parse(self.response)
         return self._extract_data(doc)
 
     @abstractmethod
@@ -132,7 +128,7 @@ class BaseScraper(ABC, Generic[ResourceType]):
             model=model.__name__,
             source_id=source_id,
             source_name=type(self).__name__,
-            source_url=self._response.request.url,
+            source_url=self.response.request.url,
             group_key=group_key,
             data=data,
         )
@@ -167,9 +163,6 @@ class BaseScraper(ABC, Generic[ResourceType]):
             "accept-language": "en-us",
             "user-agent": random.choice(USER_AGENTS),
         }
-
-    def _compute_checksum(self, response: Response) -> str:
-        return hashlib.sha256(response.content).hexdigest()
 
 
 class BeautifulSoupScraper(BaseScraper[BeautifulSoup]):
