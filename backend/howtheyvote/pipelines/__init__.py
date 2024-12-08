efa13e3fe0ede19efb0f313843e9926125ce09ec
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2024-12-08 16:16:15 +0100

    Refactor: Remove unnecessary package exports
    All three are only used by other modules from the same package

--- backend/howtheyvote/pipelines/__init__.py
+++ backend/howtheyvote/pipelines/__init__.py
@@ -1,21 +1,11 @@
-from .common import (
-    BasePipeline,
-    DataUnavailableError,
-    DataUnchangedError,
-    PipelineError,
-    PipelineResult,
-)
+from .common import PipelineResult
 from .members import MembersPipeline
 from .press import PressPipeline
 from .rcv_list import RCVListPipeline
 from .sessions import SessionsPipeline
 
 __all__ = [
-    "BasePipeline",
     "PipelineResult",
-    "PipelineError",
-    "DataUnavailableError",
-    "DataUnchangedError",
     "RCVListPipeline",
     "PressPipeline",
     "MembersPipeline",
