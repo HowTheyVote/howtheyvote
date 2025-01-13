2a30678655549a69eae7c3d8a1d78f4af0e2310d
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2025-01-13 23:46:02 +0100

    Implement search using Xapian

--- backend/howtheyvote/api/query.py
+++ backend/howtheyvote/api/query.py
@@ -1,14 +1,34 @@
 import copy
+import datetime
 import enum
 from abc import ABC, abstractmethod
 from typing import Any, Generic, Self, TypedDict, TypeVar
 
 from sqlalchemy import desc, func, select
 from sqlalchemy.sql import ColumnElement
+from xapian import (
+    BM25Weight,
+    Database,
+    Enquire,
+    QueryParser,
+    ValuePostingSource,
+    ValueWeightPostingSource,
+    Weight,
+    sortable_unserialise,
+)
+from xapian import (
+    Query as XapianQuery,
+)
 
 from ..db import Session
-from ..meili import get_index
 from ..models import BaseWithId
+from ..search import (
+    FIELD_TO_SLOT_MAPPING,
+    SLOT_IS_FEATURED,
+    SLOT_TIMESTAMP,
+    get_index,
+    get_stopper,
+)
 
 T = TypeVar("T", bound=BaseWithId)
 
@@ -168,80 +188,105 @@ class DatabaseQuery(Query[T]):
         return query
 
 
-class MeilisearchSearchParams(TypedDict):
-    limit: int
-    offset: int
-    attributesToRetrieve: list[str]
-    filter: list[str]
-    sort: list[str]
+class ValueDecayWeightPostingSource(ValuePostingSource):
+    """A posting source that returns a weight that decreases the larger the difference between
+    a given constant origin and a document value is. If the document value equals the given
+    origin, the posting source returns a weight of 1. The weight decreases linearly until the
+    maximum difference is reached, at which point the posting source returns a weight of 0.
+
+    For example, this can be used to boost recent documents by using the current time as the
+    origin.
+
+    In order to adjust the range of the returned weight, combine this with a OP_SCALE_WEIGHT
+    query. For example, to assign a weight between 0 and 0.5, use a scaling factor of 0.5.
+
+    See: https://getting-started-with-xapian.readthedocs.io/en/latest/advanced/postingsource.html
+    """
+
+    def set_max_diff(self, max_diff: float) -> None:
+        self.max_diff = max_diff
+
+    def set_origin(self, origin: float) -> None:
+        self.origin = origin
+
+    def get_weight(self) -> float:
+        value = sortable_unserialise(self.get_value())
+        diff = self.origin - value
+        weight = 1 - min(1, diff / self.max_diff)
+
+        return weight
 
 
 class SearchQuery(Query[T]):
+    BOOST_FEATURED = 0.075
+    """Constant weight added for featured votes."""
+
+    BOOST_PHRASE = 0.1
+    """Scaling factor for the phrase subquery. Every user-provided search query is also
+    converted to a phrase query to slightly boost votes matching the search query exactly."""
+
+    BOOST_AGE = 0.25
+    """Maximum weight added for recent votes. If a vote’s timestamp equals the current time
+    this weight is added. The weight decreases linearly with a vote’s age up to
+    `AGE_DECAY_DAYS`. For details, see `ValueDecayWeightPostingSource`."""
+
+    AGE_DECAY_DAYS = 365
+    """Maximum vote age in days. Older votes aren't boosted."""
+
     def __init__(self, model: type[T]):
         super().__init__(model)
         self._query: str | None = None
 
     def handle(self) -> QueryResponse[T]:
-        index = get_index(self.model)
         page = self.get_page()
         page_size = self.get_page_size()
         limit = self.get_limit()
         offset = self.get_offset()
 
-        params: MeilisearchSearchParams = {
-            # In order to determine if there is a next page, we fetch one additional
-            # result from the search index.
-            "limit": limit + 1,
-            "offset": offset,
-            # Retrieve only IDs from search index as everything else is fetched
-            # from the database
-            "attributesToRetrieve": ["id"],
-            "sort": [],
-            "filter": [],
-        }
+        with get_index(self.model) as index:
+            query = self._xapian_query(index)
+            enquire = Enquire(index)
+            enquire.set_query(query)
+            enquire.set_weighting_scheme(self._xapian_bm25_weight())
 
-        sort = self.get_sort()
-        q = self.get_query()
+            sort = self.get_sort()
+            if sort:
+                field, order = sort
 
-        if sort or not q:
-            # Apply default sorting only if none is specified explicitly and
-            # no search query is given
-            if not sort:
-                sort_field = self.DEFAULT_SORT_FIELD
-                sort_order = self.DEFAULT_SORT_ORDER
+                # Fields need to be stored as values in order to be used for sorting
+                slot = FIELD_TO_SLOT_MAPPING.get(field)
+
+                reverse = order == Order.DESC
             else:
-                sort_field, sort_order = sort
+                slot = None
 
-            params["sort"] = [f"{sort_field}:{sort_order.value}"]
+            if slot is not None:
+                enquire.set_sort_by_value(slot, reverse)
+            else:
+                enquire.set_sort_by_relevance_then_value(SLOT_TIMESTAMP, False)
 
-        for field, value in self.get_filters().items():
-            if isinstance(value, bool):
-                # Meilisearch represents booleans as integers
-                value = int(value)
-
-            params["filter"].append(f"{field} = {value}")
-
-        res = index.search(q, params)
+            # Fetch one extra result to check if there is a next page
+            mset = enquire.get_mset(offset, limit + 1)
 
         # Based on the IDs fetched from the search index, fetch full records
         # from the database
-        ids = [int(hit["id"]) for hit in res["hits"]]
+        ids = [int(match.docid) for match in mset]
 
         # Remove the extra item fetched only to test if there is a next page
         ids = ids[:limit]
 
-        query = select(self.model).where(self.model.id.in_(ids))
-        results = list(Session.execute(query).scalars())
+        db_query = select(self.model).where(self.model.id.in_(ids))
+        results = list(Session.execute(db_query).scalars())
 
         # Sort in the same order as returned in search response
         results = sorted(results, key=lambda r: ids.index(int(r.id)))
 
         response: QueryResponse[T] = {
-            "total": res["estimatedTotalHits"],
+            "total": mset.get_matches_estimated(),
             "page": page,
             "page_size": page_size,
             "has_prev": page > 1,
-            "has_next": len(res["hits"]) > limit,
+            "has_next": mset.size() > limit,
             "results": results,
         }
 
@@ -254,3 +299,91 @@ class SearchQuery(Query[T]):
 
     def get_query(self) -> str:
         return self._query or ""
+
+    def _xapian_query_parser(self, index: Database) -> QueryParser:
+        parser = QueryParser()
+        parser.set_stopper(get_stopper())
+        parser.set_database(index)
+
+        return parser
+
+    def _xapian_query(self, index: Database) -> XapianQuery:
+        parser = self._xapian_query_parser(index)
+        query = parser.parse_query(self.get_query())
+
+        if query.empty():
+            query = XapianQuery.MatchAll
+        else:
+            query = XapianQuery(
+                XapianQuery.OP_AND_MAYBE,
+                query,
+                self._xapian_featured_subquery(),
+            )
+
+            query = XapianQuery(
+                XapianQuery.OP_AND_MAYBE,
+                query,
+                self._xapian_age_subquery(),
+            )
+
+            query = XapianQuery(
+                XapianQuery.OP_AND_MAYBE,
+                query,
+                self._xapian_phrase_subquery(index),
+            )
+
+        return query
+
+    def _xapian_phrase_subquery(self, index: Database) -> XapianQuery:
+        # This is a phrase subquery, i.e. it matches documents that contain the terms of the
+        # search query in the original order. It's used to boost phrase matches even if
+        # a user hasn't explicitly specified a phrase query.
+        parser = self._xapian_query_parser(index)
+        parser.set_default_op(XapianQuery.OP_PHRASE)
+        query = parser.parse_query(self.get_query())
+
+        return XapianQuery(
+            XapianQuery.OP_SCALE_WEIGHT,
+            query,
+            self.BOOST_PHRASE,
+        )
+
+    def _xapian_featured_subquery(self) -> XapianQuery:
+        # This subquery assigns a constant weight to featured votes and 0 otherwise.
+        return XapianQuery(
+            XapianQuery.OP_SCALE_WEIGHT,
+            XapianQuery(ValueWeightPostingSource(SLOT_IS_FEATURED)),
+            self.BOOST_FEATURED,
+        )
+
+    def _xapian_age_subquery(self) -> XapianQuery:
+        # This subquery assigns a decreasing weight based on age, i.e. recent votes get a
+        # higher weight.
+        now = datetime.datetime.now().timestamp()
+        max_diff = datetime.timedelta(days=self.AGE_DECAY_DAYS).total_seconds()
+
+        age_source = ValueDecayWeightPostingSource(SLOT_TIMESTAMP)
+        age_source.set_max_diff(max_diff)
+        age_source.set_origin(now)
+
+        return XapianQuery(
+            XapianQuery.OP_SCALE_WEIGHT,
+            XapianQuery(age_source),
+            self.BOOST_AGE,
+        )
+
+    def _xapian_bm25_weight(self) -> Weight:
+        # This parameter controls document length normalization. As we currently index mostly
+        # short vote titles and a few keywords, we set b=0 to disable normalization. We do
+        # not want a vote to score higher just because its title is shorter. If that changes
+        # (and we start indexing longer documents), we should reconsider this.
+        b = 0  # Xapian default: 0.5
+
+        # The following BM25 parameters are Xapian’s defaults.
+        # See: https://xapian.org/docs/apidoc/html/classXapian_1_1BM25Weight.html
+        k1 = 1
+        k2 = 0
+        k3 = 1
+        min_normlen = 0.5
+
+        return BM25Weight(k1, k2, k3, b, min_normlen)
--- backend/howtheyvote/store/index.py
+++ backend/howtheyvote/store/index.py
@@ -1,13 +1,25 @@
 from collections.abc import Iterable, Iterator
-from typing import Literal, TypedDict, TypeVar, cast
+from typing import TypeVar, cast
 
 from sqlalchemy.dialects.sqlite import insert
 from structlog import get_logger
+from xapian import Document, TermGenerator, sortable_serialise
 
 from ..db import Session
 from ..helpers import chunks
-from ..meili import votes_index
 from ..models import BaseWithId, Vote
+from ..search import (
+    BOOST_DISPLAY_TITLE,
+    BOOST_EUROVOC_CONCEPTS,
+    BOOST_GEO_AREAS,
+    PREFIX_PROCEDURE_REFERENCE,
+    PREFIX_REFERENCE,
+    SLOT_IS_FEATURED,
+    SLOT_TIMESTAMP,
+    AccessType,
+    get_index,
+    get_stopper,
+)
 
 log = get_logger(__name__)
 
@@ -64,64 +76,83 @@ def index_db(model_cls: type[RecordType], records: Iterable[RecordType]) -> None
 def index_search(
     model_cls: type[RecordType],
     records: Iterable[RecordType],
-    sync: bool = False,
 ) -> None:
-    # At the moment, only votes are indexed in Meilisearch
+    # At the moment, only votes are searchable
     if model_cls != Vote:
         return
 
     votes = cast(Iterable[Vote], records)
-    formatted_records = []
+    filtered_votes = [vote for vote in votes if vote.is_main and vote.display_title]
 
-    for vote in votes:
-        if vote.is_main and vote.display_title:
-            formatted_records.append(_serialize_vote(vote))
-
-    if not len(formatted_records):
+    if not filtered_votes:
         log.warning("Skipping indexing to search index as list of records is empty")
         return
 
-    log.info("Writing aggregated records to search index", count=len(formatted_records))
+    log.info("Indexing aggregated records", count=len(filtered_votes))
 
-    # `Index.add_documents` requires `list[dict[str, any]]` which is incompatible with
-    # the `SerializedVote` typed dict. See https://github.com/python/mypy/issues/4976
-    documents = [dict(td) for td in formatted_records]
-    task = votes_index.add_documents(documents)
+    with get_index(Vote, AccessType.WRITE) as index:
+        generator = TermGenerator()
+        generator.set_database(index)
+        generator.set_stopper(get_stopper())
+        generator.set_stopper_strategy(TermGenerator.STOP_ALL)
 
-    if sync:
-        # This is primarily used in tests
-        votes_index.wait_for_task(task.task_uid)
+        # Automatically add words from indexed documents to spelling dictionary to
+        # enable spelling correction
+        generator.set_flags(TermGenerator.FLAG_SPELLING)
 
-
-class SerializedVote(TypedDict):
-    id: int
-    timestamp: float
-    display_title: str | None
-    reference: str | None
-    procedure_reference: str | None
-    description: str | None
-    is_featured: Literal[0, 1]
-    geo_areas: list[str]
-    keywords: list[str]
+        for vote in filtered_votes:
+            doc = _serialize_vote(vote, generator)
+            index.replace_document(int(vote.id), doc)
 
 
-def _serialize_vote(vote: Vote) -> SerializedVote:
-    keywords = set()
+def _serialize_vote(vote: Vote, generator: TermGenerator) -> Document:
+    doc = Document()
+    generator.set_document(doc)
 
+    generator.index_text(vote.display_title, BOOST_DISPLAY_TITLE)
+
+    # Calling this method between indexing of different fields prevents
+    # searches matching terms from different fields, (e.g. last term of
+    # the title and first term of the following field).
+    generator.increase_termpos()
+
+    # Index EuroVoc concepts for full-text search
     for concept in vote.eurovoc_concepts:
-        keywords.add(concept.label)
-        keywords.update(concept.alt_labels)
-        keywords.update(bc.label for bc in concept.broader)
+        for term in set([concept.label, *concept.alt_labels]):
+            generator.index_text(term, BOOST_EUROVOC_CONCEPTS)
+            generator.increase_termpos()
 
-    return {
-        "id": vote.id,
-        # Meilisearch requires dates to be indexed as a numeric timestamp
-        "timestamp": vote.timestamp.timestamp(),
-        "display_title": vote.display_title,
-        "reference": vote.reference,
-        "procedure_reference": vote.procedure_reference,
-        "description": vote.description,
-        "is_featured": 1 if vote.is_featured else 0,
-        "geo_areas": [country.label for country in vote.geo_areas],
-        "keywords": list(keywords),
-    }
+        for broader in concept.broader:
+            for term in set([broader.label, *broader.alt_labels]):
+                # Index broader concepts, too, but do not boost them as this may
+                # cause too many high-ranking false positives
+                generator.index_text(term)
+                generator.increase_termpos()
+
+    # Index geographic areas for full-text search
+    for geo_area in vote.geo_areas:
+        generator.index_text(geo_area.label, BOOST_GEO_AREAS)
+        generator.increase_termpos()
+
+    # Index rapporteur name
+    if vote.rapporteur:
+        generator.index_text(vote.rapporteur)
+
+    # Store timestamp and is_featured as sortable values for ranking
+    timestamp = sortable_serialise(vote.timestamp.timestamp())
+    doc.add_value(SLOT_TIMESTAMP, timestamp)
+
+    is_featured = sortable_serialise(int(vote.is_featured))
+    doc.add_value(SLOT_IS_FEATURED, is_featured)
+
+    # Store document and procedure references as boolean terms. Boolean terms
+    # aren’t searchable, but can be used for filtering.
+    if vote.reference:
+        term = f"{PREFIX_REFERENCE}{vote.reference.lower()}"
+        doc.add_boolean_term(term)
+
+    if vote.procedure_reference:
+        term = f"{PREFIX_PROCEDURE_REFERENCE}{vote.procedure_reference.lower()}"
+        doc.add_boolean_term(term)
+
+    return doc
