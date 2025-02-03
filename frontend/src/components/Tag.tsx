c720f29614bb767e7598e7543fcff482bf371985
linusha <mail@linushagemann.de>
2025-02-03 17:28:46 +0100

    Remove "Featured" indicator in frontend

--- frontend/src/components/Tag.tsx
+++ frontend/src/components/Tag.tsx
@@ -1,15 +1,13 @@
 import type { ComponentChildren } from "preact";
-import { bem } from "../lib/bem";
 
 import "./Tag.css";
 
 type TagProps = {
-  style?: "featured";
   children: ComponentChildren;
 };
 
-function Tag({ style, children }: TagProps) {
-  return <span className={bem("tag", style)}>{children}</span>;
+function Tag({ children }: TagProps) {
+  return <span className="tag">{children}</span>;
 }
 
 export default Tag;
