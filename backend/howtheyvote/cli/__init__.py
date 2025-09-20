3ac1e47b08068e9fad8f88c8593abbc0fe5907dd
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2025-09-20 14:24:55 +0200

    Add CLI command to delete fragments
    Pipelines/scrapers only insert or update fragments, but they never delete them. That means if a fragment has been written erroneously (either because of an error in the source data or because of a bug in the software) it has to be deleted explicitly.
    
    This adds a CLI command to manually delete fragments. Usage examples:
    
    ```
    htv flush votes --id 12345
    htv flush votes --id 12345 --id 67890
    htv flush votes --date 2025-09-20
    htv flush votes --source-name RCVListScraper
    htv flush votes --source-name RCVListScraper --id 12345
    ```

--- backend/howtheyvote/cli/__init__.py
+++ backend/howtheyvote/cli/__init__.py
@@ -11,6 +11,7 @@ from ..sharepics import generate_vote_sharepic
 from ..worker import worker as worker_
 from .aggregate import aggregate
 from .dev import dev
+from .flush import flush
 from .pipeline import pipeline
 from .system import system
 from .temp import temp
@@ -39,3 +40,4 @@ cli.add_command(aggregate)
 cli.add_command(pipeline)
 cli.add_command(dev)
 cli.add_command(temp)
+cli.add_command(flush)
