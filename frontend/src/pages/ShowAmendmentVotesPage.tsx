4282cc150fca1ae460b1c5dce6045e1bb2b665cc
linusha <mail@linushagemann.de>
2025-09-24 21:28:54 +0200

    Improve header for amendment page and add short disclaimer text

--- frontend/src/pages/ShowAmendmentVotesPage.tsx
+++ frontend/src/pages/ShowAmendmentVotesPage.tsx
@@ -1,18 +1,17 @@
 import { getVote, type Vote } from "../api";
+import AmendmentHeader from "../components/AmendmentHeader";
 import AmendmentVoteCards from "../components/AmendmentVoteCards";
 import App from "../components/App";
 import BaseLayout from "../components/BaseLayout";
 import Callout from "../components/Callout";
 import Stack from "../components/Stack";
-import VoteHeader from "../components/VoteHeader";
 import Wrapper from "../components/Wrapper";
 import type { Loader, Page, Request } from "../lib/server";
 
 export const loader: Loader<Vote> = async (request: Request) => {
   const { data } = await getVote({ path: { vote_id: request.params.id } });
-
   // The last element of `related` is the vote itself
-  if (data.related[data.related.length - 1].id === Number(request.params.id))
+  if (data.related[data.related.length - 1].id === request.params.id)
     data.related = data.related.slice(0, -1);
 
   return data;
@@ -23,16 +22,20 @@ export const ShowAmendmentVotesPage: Page<Vote> = ({ data }) => {
     <App title={[data.display_title, "Amendments"]}>
       <BaseLayout>
         <Stack space="lg">
-          <VoteHeader vote={data} />
+          <AmendmentHeader vote={data} />
           <Wrapper>
-            <Callout title="Hello">
+            <Callout>
               <p>
-                The votes are <b>amendments</b>. Amendments change the final
-                text that parliament votes on, but do not produce Parliament
-                decisions on their own. You can see the result of the final vote{" "}
-                <a href={`/votes/${data.id}`}>here</a>
+                The votes on this page were on <b>amendments</b>. Amendments aim
+                at changing the text of reports.<br /> The outcome of amendments do
+                not dictate the outcome of the vote on a report.<br /> Successful
+                amendments do not become part of official Parliament positions
+                if the report is not accepted in its{" "}
+                <a href={`/votes/${data.id}`}>final vote</a>.
               </p>
             </Callout>
+          </Wrapper>
+          <Wrapper>
             <div className="px">
               <AmendmentVoteCards votes={data.related} />
             </div>
