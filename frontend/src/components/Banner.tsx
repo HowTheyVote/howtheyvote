86dafd53fc6a82775014be38f6839fee308d52c7
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2025-10-07 19:59:06 +0200

    Implement reusable icon component

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -1,4 +1,5 @@
 import { useState } from "preact/hooks";
+import Icon from "./Icon";
 import Wrapper from "./Wrapper";
 
 import "./Banner.css";
@@ -35,14 +36,7 @@ export default function Banner() {
         </a>
         <button type="button" className="Banner__close" onClick={onClose}>
           <span class="visually-hidden">Close</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            viewBox="0 0 16 16"
-            aria-hidden="true"
-          >
-            <path d="M14.7,1.3c-0.4-0.4-1-0.4-1.4,0L8,6.6L2.7,1.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4L6.6,8l-5.3,5.3c-0.4,0.4-0.4,1,0,1.4C1.5,14.9,1.7,15,2,15s0.5-0.1,0.7-0.3L8,9.4l5.3,5.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3c0.4-0.4,0.4-1,0-1.4L9.4,8l5.3-5.3C15.1,2.3,15.1,1.7,14.7,1.3z" />
-          </svg>
+          <Icon name="close" className="Banner__icon" />
         </button>
       </Wrapper>
     </div>
--- frontend/src/components/Select.tsx
+++ frontend/src/components/Select.tsx
@@ -1,4 +1,5 @@
 import type { JSX } from "preact";
+import Icon from "./Icon";
 
 import "./Select.css";
 
@@ -14,18 +15,7 @@ export default function Select({ options, value = "", ...rest }: SelectProps) {
         {options[value]}
       </span>
 
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        class="select__icon"
-        aria-hidden="true"
-      >
-        <path
-          fillRule="evenodd"
-          d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
-          clipRule="evenodd"
-        />
-      </svg>
+      <Icon name="chevron-down" />
 
       <select autoComplete="off" {...rest}>
         {Object.entries(options).map(([optionValue, label]) => (
