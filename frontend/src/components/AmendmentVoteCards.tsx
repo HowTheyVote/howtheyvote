6eb00d88324e0c7613860ac6d6c2ad4bac062f25
linusha <mail@linushagemann.de>
2025-10-11 19:47:48 +0200

    Remove currently unused grouping feature from `AmendmentVoteCards`

--- frontend/src/components/AmendmentVoteCards.tsx
+++ frontend/src/components/AmendmentVoteCards.tsx
@@ -1,5 +1,4 @@
 import type { RelatedVote } from "../api";
-import { formatDate } from "../lib/dates";
 import AmendmentVoteCard from "./AmendmentVoteCard";
 import Stack from "./Stack";
 
@@ -7,20 +6,15 @@ import "./VoteCards.css";
 
 type AmendmentVoteCardsProps = {
   votes: Array<RelatedVote>;
-  groupByDate?: boolean;
 };
 
 type GroupProps = {
   votes: Array<RelatedVote>;
-  // To group by political Group later
-  title?: string;
 };
 
-function Group({ votes, title }: GroupProps) {
+function Group({ votes }: GroupProps) {
   return (
     <Stack space="sm">
-      {title && <h2 class="delta">{title}</h2>}
-
       {votes.map((vote: RelatedVote) => (
         <AmendmentVoteCard key={vote.id} vote={vote} />
       ))}
@@ -28,36 +22,10 @@ function Group({ votes, title }: GroupProps) {
   );
 }
 
-export default function AmendmentVoteCards({
-  votes,
-  groupByDate,
-}: AmendmentVoteCardsProps) {
-  if (!groupByDate) {
-    return (
-      <div class="vote-cards">
-        <Group votes={votes} />
-      </div>
-    );
-  }
-
-  const groups = new Map<string, Array<RelatedVote>>();
-
-  for (const vote of votes) {
-    const key = formatDate(new Date(vote.timestamp));
-
-    if (!key) {
-      continue;
-    }
-
-    const otherVotes = groups.get(key) || [];
-    groups.set(key, [...otherVotes, vote]);
-  }
-
+export default function AmendmentVoteCards({ votes }: AmendmentVoteCardsProps) {
   return (
     <div class="vote-cards">
-      {Array.from(groups.entries()).map(([formattedDate, votes]) => (
-        <Group key={formattedDate} votes={votes} title={formattedDate} />
-      ))}
+      <Group votes={votes} />
     </div>
   );
 }
