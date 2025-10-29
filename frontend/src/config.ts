81f4fe9e1770f7b2f965c3db314051158553ef96
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2025-10-29 11:00:05 +0100

    Allow importing `config.ts` in client-side components
    Previously, this would’ve failed on the client as `process` is defined only in Node.js contexts. We don’t have a use case to access dynamic (i.e. set via env variables) config options in client-side components at the moment, so this solutions is acceptable for now.

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,15 @@
-export const PUBLIC_URL = process.env.HTV_FRONTEND_PUBLIC_URL || "";
-export const BACKEND_PRIVATE_URL = process.env.HTV_BACKEND_PRIVATE_URL || "";
-export const BACKEND_PUBLIC_URL = process.env.HTV_BACKEND_PUBLIC_URL || "";
+export const PUBLIC_URL = getEnv("HTV_FRONTEND_PUBLIC_URL", "");
+export const BACKEND_PRIVATE_URL = getEnv("HTV_BACKEND_PRIVATE_URL", "");
+export const BACKEND_PUBLIC_URL = getEnv("HTV_BACKEND_PUBLIC_URL", "");
 export const TIMEZONE = "Europe/Brussels";
+
+function getEnv(name: string): string | undefined;
+function getEnv(name: string, defaultValue: string): string;
+function getEnv(name: string, defaultValue?: string): string | undefined {
+  // `process.env` isn’t available on the client
+  if (!process?.env) {
+    return defaultValue;
+  }
+
+  return process.env[name] || defaultValue;
+}
