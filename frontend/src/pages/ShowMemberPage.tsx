204b9d64010bf4eff8094da250c3e1f23b443b60
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2025-12-05 15:30:46 +0100

    Add OpenGraph meta tags for MEP profile pages

--- frontend/src/pages/ShowMemberPage.tsx
+++ frontend/src/pages/ShowMemberPage.tsx
@@ -68,6 +68,7 @@ export const ShowMemberPage: Page<ShowMemberPageData> = ({ data, request }) => {
         "Member of the European Parliament",
         "Votes",
       ]}
+      head={<MetaTags member={data.member} />}
     >
       <BaseLayout footer={footer}>
         <Stack>
@@ -88,3 +89,21 @@ export const ShowMemberPage: Page<ShowMemberPageData> = ({ data, request }) => {
     </App>
   );
 };
+
+function MetaTags({ member }: { member: Member }) {
+  const altText = `Lorem ipsum`;
+  const title = `${member.first_name} ${member.last_name} ・ Member of the European Parliament`;
+  const description =
+    "Find out how the Members of the European Parliament vote.";
+
+  return (
+    <>
+      <meta name="description" content={description} />
+      <meta property="og:site_name" content="HowTheyVote.eu" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={member.sharepic_url} />
+      <meta property="og:image:alt" content={altText} />
+    </>
+  );
+}
