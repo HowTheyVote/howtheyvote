57ea0a4652d1e90b5d55f0c330b5ab83b0148843
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2025-12-07 15:56:53 +0100

    Rename module to follow consistent naming pattern

--- backend/howtheyvote/pipelines/__init__.py
+++ backend/howtheyvote/pipelines/__init__.py
@@ -1,9 +1,9 @@
 from .common import PipelineResult
 from .members import MembersPipeline
+from .oeil_summaries import OEILSummariesPipeline
 from .press import PressPipeline
 from .rcv_list import RCVListPipeline
 from .sessions import SessionsPipeline
-from .summaries import OEILSummariesPipeline
 from .vot_list import VOTListPipeline
 
 __all__ = [
