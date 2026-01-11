3c54dc4d3227b2f4ea8241f3db36961d3e8f6512
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2026-01-11 13:21:03 +0100

    Extract card styles into generic component
    The `VoteCard` and `AmendmentVoteCard` components previously were separate components, but include styles of the `VoteCard` components. That can cause unintended side effects when updating components.

--- frontend/src/components/AmendmentVoteCard.tsx
+++ frontend/src/components/AmendmentVoteCard.tsx
@@ -1,7 +1,6 @@
 import type { RelatedVote } from "../api";
 import { formatDate } from "../lib/dates";
-
-import "./VoteCard.css";
+import Card from "./Card";
 
 type AmendmentVoteCardProps = {
   vote: RelatedVote;
@@ -9,15 +8,10 @@ type AmendmentVoteCardProps = {
 
 export default function AmendmentVoteCard({ vote }: AmendmentVoteCardProps) {
   return (
-    <article class="vote-card">
-      <div class="vote-card__text">
-        <h2 class="vote-card__title">
-          <a href={`/votes/${vote.id}`}>{vote.description}</a>
-        </h2>
-        <div class="vote-card__meta">
-          {formatDate(new Date(vote.timestamp))}
-        </div>
-      </div>
-    </article>
+    <Card
+      title={vote.description}
+      link={`/votes/${vote.id}`}
+      meta={formatDate(new Date(vote.timestamp))}
+    />
   );
 }
