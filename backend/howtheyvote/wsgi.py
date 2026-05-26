14f9782c78e009dfb2ebed99d7b119bd8c25d6f9
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2026-05-26 21:50:54 +0200

    Log API requests

--- backend/howtheyvote/wsgi.py
+++ backend/howtheyvote/wsgi.py
@@ -1,9 +1,10 @@
+import time
 from typing import Any
 
-from flask import Flask, Response, jsonify, request, request_started
+import sentry_sdk
+from flask import Flask, Response, g, jsonify, request
 from flask.typing import ResponseReturnValue
 from structlog import get_logger
-from structlog.contextvars import bind_contextvars, clear_contextvars
 from werkzeug.exceptions import HTTPException
 from werkzeug.middleware.proxy_fix import ProxyFix
 
@@ -31,19 +32,6 @@ def json_error_handler(error: HTTPException) -> ResponseReturnValue:
 app.register_error_handler(HTTPException, json_error_handler)
 
 
-# Add request info to logs
-def bind_request_details(_: Flask) -> None:
-    clear_contextvars()
-    bind_contextvars(
-        path=request.path,
-        query_string=request.query_string.decode("utf-8"),
-        endpoint=request.url_rule.endpoint if request.url_rule else None,
-    )
-
-
-request_started.connect(bind_request_details, app)
-
-
 # We run the application behind Caddy both in dev and production environments.
 # https://caddyserver.com/docs/caddyfile/directives/reverse_proxy#defaults
 # mypy complains due to monkey-patching: https://github.com/python/mypy/issues/7347
@@ -54,6 +42,37 @@ app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1)  # type: ign
 @app.teardown_appcontext
 def teardown_session(_: Any) -> None:
     Session.remove()
+
+
+# Request logs/metrics
+@app.before_request
+def before_request() -> None:
+    g.start_time = time.perf_counter()
+
+
+@app.after_request
+def after_request(response: Response) -> Response:
+    # perf_counter measures time in seconds
+    request_duration = (time.perf_counter() - g.start_time) * 1000
+
+    attributes = {
+        "method": request.method,
+        "status": response.status_code,
+        "route": request.url_rule.endpoint if request.url_rule else None,
+        "path": request.path,
+        "query_string": request.query_string.decode("utf-8"),
+    }
+
+    log.info("Handled request", **attributes, request_duration=round(request_duration))
+    sentry_sdk.metrics.count("requests_handled", 1, attributes=attributes)
+    sentry_sdk.metrics.distribution(
+        "request_duration",
+        request_duration,
+        attributes=attributes,
+        unit="milliseconds",
+    )
+
+    return response
 
 
 # CORS
