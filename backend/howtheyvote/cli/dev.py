11cadd159a5bd045b76920efd9e91b938cb7f171
Linus Hagemann <linus.hagemann@posteo.net>
2026-07-23 17:54:05 +0200

    Reduce number of national parties which are rescraped

--- backend/howtheyvote/cli/dev.py
+++ backend/howtheyvote/cli/dev.py
@@ -545,18 +545,24 @@ def load_national_parties() -> None:
 
     all_parties_response = requests.get("https://data.europarl.europa.eu/api/v2/corporate-bodies?body-classification=NATIONAL_POLITICAL_GROUP&format=application/ld+json&offset=0", timeout=60).json()
     all_parties = all_parties_response["data"]
-    print(f"Loading data for {len(all_parties)} parties")
+    print(f"Got data for {len(all_parties)} national parties from ODP")
 
-    for party in all_parties:
-        curr_party_identifier = party["identifier"]
+    # We need to retrieve info for parties that we do not yet have locally.
+    # As we do not know for sure how renamings are handled by the ODP, also rescrape info for active parties.    
+    party_identifier_to_scrape = [
+       party["identifier"]
+        for party in all_parties
+        if (retrieved := retrieved_parties.get(party["identifier"])) is None
+        or retrieved.end_date is None
+    ]
+    print(f"Scraping data for {len(party_identifier_to_scrape)} parties which are new or still active.")
+    for identifier in party_identifier_to_scrape:
         # TODO: proper error handling
-        # TODO: non-existent and non-ending parties here
-        if not any(p.id == curr_party_identifier for p in retrieved_parties):
-            try:
-                party_info = ODPNationalPartyScraper(id=party["identifier"]).run()
-                retrieved_parties.add(party_info)
-            except: 
-                retrieved_parties.save()            
+        try:
+            party_info = ODPNationalPartyScraper(id=identifier).run()
+            retrieved_parties.add(party_info)
+        except: 
+            retrieved_parties.save()            
     retrieved_parties.save()
 
 
