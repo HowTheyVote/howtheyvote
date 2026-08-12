cb086bb1c61f207f750e65a70f1f9a29c7e34a3c
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2026-08-12 19:42:54 +0200

    Handle edge case of a vote being about multiple amendments

--- backend/howtheyvote/api/openapi_spec.py
+++ backend/howtheyvote/api/openapi_spec.py
@@ -5,6 +5,7 @@ from .. import config
 from .openapi_helpers import get_schema, normalize_schema_name
 from .serializers import (
     AmendmentAuthorDict,
+    AmendmentURLDict,
     BaseMemberDict,
     BaseVoteDict,
     BaseVoteWithMemberPositionDict,
@@ -85,6 +86,7 @@ schema_classes = [
     FacetOptionDict,
     VotesQueryResponseDict,
     MemberVotesQueryResponseDict,
+    AmendmentURLDict,
     RelatedVoteDict,
     Statistics,
     SnippetDict,
