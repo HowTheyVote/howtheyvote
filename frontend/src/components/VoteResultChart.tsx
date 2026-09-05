HowTheyVote/howtheyvote

commit 24b1d4d563aabb6dca50e75aa2bc80204108a2c8

Fix inconsistent formatting
No visible difference, but it annoyed me

frontend/src/components/VoteResultChart.tsx
@@ -51,12 +51,12 @@ function Summary({ stats }: SummaryProps) {
 
   return (
     <p class="text--sm text--light" data-testid="vote-result-chart-summary">
-      For: <strong>{stats.FOR}</strong>
-      <span class="visually-hidden">({percentageFor} %)</span>. Against:{" "}
-      <strong>{stats.AGAINST}</strong>
-      <span class="visually-hidden">({percentageAgainst} %)</span>. Abstentions:{" "}
-      <strong>{stats.ABSTENTION}</strong>
-      <span class="visually-hidden">({percentageAbstention} %)</span>. In total,{" "}
+      For: <strong>{stats.FOR}</strong>{" "}
+      <span class="visually-hidden">({percentageFor}%)</span>. Against:{" "}
+      <strong>{stats.AGAINST}</strong>{" "}
+      <span class="visually-hidden">({percentageAgainst}%)</span>. Abstentions:{" "}
+      <strong>{stats.ABSTENTION}</strong>{" "}
+      <span class="visually-hidden">({percentageAbstention}%)</span>. In total,{" "}
       <strong>{total} MEPs</strong> voted.{" "}
       <strong>{stats.DID_NOT_VOTE} MEPs</strong> didn’t vote.
     </p>

frontend/src/components/VoteResultChart.test.tsx
@@ -25,7 +25,7 @@ describe("VoteResultChart", () => {
 
     assert.strictEqual(
       summary.textContent,
-      "For: 5(50 %). Against: 3(30 %). Abstentions: 2(20 %). In total, 10 MEPs voted. 1 MEPs didn’t vote.",
+      "For: 5 (50%). Against: 3 (30%). Abstentions: 2 (20%). In total, 10 MEPs voted. 1 MEPs didn’t vote.",
     );
   });
 });

commit 6b550e206265fac52edccbe1b18f6f4cac28bff1

Fix visual regression
The change introduced in 24b1d4d5 caused a visual regression causing a space between the number of votes and the dot. Moving the spaces into the visually hidden element solves that

frontend/src/components/VoteResultChart.tsx
@@ -54,12 +54,12 @@ function Summary({ stats }: SummaryProps) {
 
   return (
     <p class="text--sm text--light" data-testid="vote-result-chart-summary">
-      For: <strong>{stats.FOR}</strong>{" "}
-      <span class="visually-hidden">({percentageFor}%)</span>. Against:{" "}
-      <strong>{stats.AGAINST}</strong>{" "}
-      <span class="visually-hidden">({percentageAgainst}%)</span>. Abstentions:{" "}
-      <strong>{stats.ABSTENTION}</strong>{" "}
-      <span class="visually-hidden">({percentageAbstention}%)</span>. In total,{" "}
+      For: <strong>{stats.FOR}</strong>
+      <span class="visually-hidden"> ({percentageFor}%)</span>. Against:{" "}
+      <strong>{stats.AGAINST}</strong>
+      <span class="visually-hidden"> ({percentageAgainst}%)</span>. Abstentions:{" "}
+      <strong>{stats.ABSTENTION}</strong>
+      <span class="visually-hidden"> ({percentageAbstention}%)</span>. In total,{" "}
       <strong>{total} MEPs</strong> voted.{" "}
       <strong>{stats.DID_NOT_VOTE} MEPs</strong> didn’t vote.
     </p>